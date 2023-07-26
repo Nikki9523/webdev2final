c478aae5270de8e5fffd2745c9517933833ad4a6
Nicola Stack <nicolastack16@gmail.com>
2023-07-26 16:21:52 +0100

    added mintemp and started max temp

--- utils/weathertop-analytics.js
+++ utils/weathertop-analytics.js
@@ -8,4 +8,25 @@ export const weathertopAnalytics = {
       }
       return latestReading;
     },
-  };
+
+  minTemp(station) {
+    let minTemp = station.readings[0].temp;
+    for (let i = 0; i < station.readings.length; i++) {
+      if (station.readings[i].temp < minTemp) {
+         minTemp = station.readings[i].temp;
+      }
+    }
+    return minTemp;
+  },
+
+  maxTemp(station) {
+    const maxTempTest = 3;
+   /* let maxTemp = station.readings[0].temp;
+    for (let i = 0; i < station.readings.length; i++) {
+      if (station.readings[i].temp > maxTemp) {
+         maxTemp = station.readings[i].temp;
+      }
+    } */
+    return maxTempTest;
+  } 
+}
