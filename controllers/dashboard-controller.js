da9d9c4f966f5094819b0d225f7f5726f4840a90
Nicola Stack <nicolastack16@gmail.com>
2023-08-12 14:37:11 +0100

    remove unused code

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,6 +1,5 @@
 import { stationStore } from "../models/station-store.js";
 import { accountsController } from "./account-controller.js";
-import axios from "axios";
 
 export const dashboardController = {
 
@@ -47,36 +46,4 @@ export const dashboardController = {
     };
     response.render("dashboard-view", viewData);
   },
-
-  async addreport(request, response) {
-    console.log("rendering new report");
-    let report = {};
-    const lat = request.body.lat;
-    const lng = request.body.lng;
-    const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=876f7ff7184f8ea886ab8a25dbece01d`
-    const result = await axios.get(requestUrl);
-    if (result.status == 200) {
-      const reading = result.data.current;
-      report.code = reading.weather[0].id;
-      report.temperature = reading.temp;
-      report.windSpeed = reading.wind_speed;
-      report.pressure = reading.pressure;
-      report.windDirection = reading.wind_deg;
-
-      report.tempTrend = [];
-      report.trendLabels = [];
-      const trends = result.data.daily;
-      for (let i = 0; i < trends.length; i++) {
-        report.tempTrend.push(trends[i].temp.day);
-        const date = new Date(trends[i].dt * 1000);
-        report.trendLabels.push(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`);
-      }
-    }
-    console.log(report);
-    const viewData = {
-      title: "Weather Report",
-      reading: report
-    };
-    response.render("dashboard-view", viewData);
-  }
 };
